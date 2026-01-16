1e2acd1da041e9da25006b7230b451890843882d
jordanbullenLAA <jordan.bullen1@justice.gov.uk>
2026-01-16 15:56:46 +0000

    Update session data access and condition checks for completeness in routing and task list

--- app/routes/v10/routing.js
+++ app/routes/v10/routing.js
@@ -61,13 +61,13 @@ router.post(version + 'evidence-capital', function (req, res) {
 
 // Check answers - evidence 
 router.post(version + 'check-answers-evidence', function (req, res) {
-    req.session.data.evidenceComplete = true;
+    req.session.data['evidenceComplete'] = true;
     res.redirect(version + 'task-list-means-complete');
   });
 
 // Client declaration
 router.post(version + 'client-declaration', function (req, res) {
-    req.session.data.declarationComplete = true;
+    req.session.data['declarationComplete'] = true;
     res.redirect(version + 'task-list-means-complete');
   });
 
@@ -75,15 +75,14 @@ router.post(version + 'client-declaration', function (req, res) {
 // GET for task list
 router.get(version + 'task-list-means-complete', function (req, res) {
     res.render(version + 'task-list-means-complete', {
-        evidenceComplete: req.session.data.evidenceComplete,
-        declarationComplete: req.session.data.declarationComplete
+        data: req.session.data,
     });
 });
 
 
 // Confirmation page
 router.post(version + 'confirmation', function (req, res) {
-    req.session.data.caseComplete = 'true';
+    req.session.data['caseComplete'] = true;
     res.redirect(version + 'case-list');
   });
 
